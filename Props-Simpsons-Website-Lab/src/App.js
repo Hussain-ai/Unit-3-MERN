9b6a2b9574052810edc9597b234ee548f16a92b1
Hussain-ai <huser.adraj@gmail.com>
2022-11-02 16:58:28 +0300

    update 1

--- Props-Simpsons-Website-Lab/src/App.js
+++ Props-Simpsons-Website-Lab/src/App.js
@@ -15,11 +15,11 @@ function App() {
       <section className="blog-items team-cards">
           <div className="inner-wrapper">
             <div className="blog-container">
-              <ProfileCard pic={homer} />
-              <ProfileCard pic={lisa} />
-              <ProfileCard />
-              <ProfileCard />
-              <ProfileCard pic={bart} />
+              <ProfileCard pic={homer} name = {"Homer"} description={"Homer like donuts"} email={"Homer@outlook.com"} />
+              <ProfileCard pic={lisa} name= {"Lisa"} description={"lisa like books"}  email={"Lisa@outlook.com"}/>
+              <ProfileCard pic={maggie} name={"maggie"} description={"maggie like to sleep"}  email={"Maggie@outlook.com"}/>
+              <ProfileCard pic={marge} name={"marge"} description={"marge like to cook"}  email={"Marge@outlook.com"}/>
+              <ProfileCard pic={bart} name={"marge"} description={"bart like to skateboard"}  email={"Bart@outlook.com"} />
             </div>
           </div>
       </section>
