d9d1af9858b94658c3be2edde45aeb48e0603bd8
Hussain-ai <huser.adraj@gmail.com>
2022-11-13 13:28:33 +0300

    last update

--- FullStack-Twitter-Student/backend lab/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-console.log('This is my database url!', process.env.DATABASE_URL)
-mongoose.connect(process.env.DATABASE_URL)
-
-mongoose.connection.on('connected', ()=>{
-    console.log('Connected to database!')
-})
